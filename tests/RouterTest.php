d35e88a93c81c383d2680a79cc5d44306ee96758
AdriUMA <adriantm@uma.es>
2023-03-27 00:09:58 +0200

    Mock builder

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -2,24 +2,31 @@
 
 namespace Xyron\Tests;
 
-use Exception;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\TestStatus\Warning;
 use Xyron\HttpMethod;
 use Xyron\HttpNotFoundException;
 use Xyron\Request;
 use Xyron\Router;
+use Xyron\Server;
 
 use function PHPUnit\Framework\assertEquals;
 
 class RouterTest extends TestCase {
+    private function createMockRequest(string $uri, HttpMethod $method): Request {
+        $mockServer = $this->getMockBuilder(Server::class)->getMock();
+        $mockServer->method("getUri")->willReturn($uri);
+        $mockServer->method("getMethod")->willReturn($method);
+    
+        return new Request($mockServer);
+    }
+
     public function test_resolve_basic_router_with_callback_action(){
         $uri = "/test";
         $action = fn() => "test";
         $router = new Router();
         $router->add(HttpMethod::GET, $uri, $action);
 
-        $this->assertEquals($action, $router->resolve(new Request(new MockServer($uri, HttpMethod::GET))));
+        $this->assertEquals($action, $router->resolve($this->createMockRequest($uri, HttpMethod::GET)));
     }
 
     public function test_resolve_multiple_basic_router_with_callback_action_for_all_http_methods(){
@@ -39,7 +46,7 @@ class RouterTest extends TestCase {
             
         foreach ($routes as $uri => $action) 
             foreach(HttpMethod::cases() as $method)
-                assertEquals($action, $router->resolve(new Request(new MockServer($uri, $method))));
+                assertEquals($action, $router->resolve($this->createMockRequest($uri, $method)));
     }
 
     public function test_resolve_unknown_routes_for_all_http_methods(){
@@ -58,7 +65,7 @@ class RouterTest extends TestCase {
         foreach ($routes as $uri => $action) 
         foreach(HttpMethod::cases() as $method){
             try{
-                $router->resolve(new Request(new MockServer($uri, $method)));
+                $router->resolve($this->createMockRequest($uri, $method));
             }catch(HttpNotFoundException){
                 $exceptions++;
             }
